e87879c8515dffd3d6529ffb2263b0a57b477ef0
Jose Gomez <210562@utxicotepec.edu.mx>
2024-07-05 14:09:19 -0600

    conexion de persons

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user
+from routes.personas import person
 from routes.persona import persona
 from routes.usuarios import usuarios
 
 app=FastAPI()
-app.include_router(persona,prefix="/api/personas",tags=["Personas"])
-app.include_router(usuarios,prefix="/api/usuarios",tags=["usuarios"])
+#app.include_router(persona,prefix="/api/personas",tags=["Personas"])
+#app.include_router(usuarios,prefix="/api/usuarios",tags=["usuarios"])
 
 app.include_router(user)
+app.include_router(person)
 print("Bienvenido a mi aplicacion")
